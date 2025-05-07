ab4e29a379988e4096f5d67d5dd508600d31ea45
afermanx <alexjferman@gmail.com>
2025-05-07 09:49:21 -0400

    feat: aadiciona estrutura de dados

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('cpf')->unique();
             $table->string('password');
+            $table->string('status')->default('active');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
